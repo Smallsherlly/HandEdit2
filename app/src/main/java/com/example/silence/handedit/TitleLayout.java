b0c905b9774865552c6fc97bdd2fb5a7fdb92168
silence <779854387@qq.com>
2018-03-06 13:23:37 +0800

    添加了自定义控件

--- app/src/main/java/com/example/silence/handedit/TitleLayout.java
+++ app/src/main/java/com/example/silence/handedit/TitleLayout.java
@@ -1,9 +1,13 @@
 package com.example.silence.handedit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by Silence on 2018/3/5.
@@ -13,5 +17,19 @@ public class TitleLayout extends LinearLayout {// 自定义布局并在构造函
     public TitleLayout(Context context, AttributeSet attrs){
         super(context,attrs);
         LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack = (Button) findViewById(R.id.title_back);
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"You clicked Edit button",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
